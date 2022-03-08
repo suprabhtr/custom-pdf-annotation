8268a4e43982366870b317d88238368bbf5826c4
Jain <C284295@ead.thomsonreuters.com>
2022-03-08 06:03:03 +0000

    Adding the PDF extended module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { PdfViewerModule } from 'ng2-pdf-viewer';
 import { ColorPickerModule } from 'ngx-color-picker';
-import { NgxExtendedPdfViewerModule } from 'ngx-extended-pdf-viewer';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CPdfViewerComponent } from './c-pdf-viewer/c-pdf-viewer.component';
@@ -16,7 +15,6 @@ import { CPdfViewerComponent } from './c-pdf-viewer/c-pdf-viewer.component';
     PdfViewerModule,
     ColorPickerModule,
     FormsModule,
-    NgxExtendedPdfViewerModule
   ],
   providers: [],
   bootstrap: [AppComponent],
